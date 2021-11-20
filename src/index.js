79f6151f0b3b4b6c6ff771cdf12ede35ec9b6a9c
PrajaktaKada <prajaktakadam0106@.com>
2021-11-20 14:56:56 +0530

    assignmentMiddleware

--- src/index.js
+++ src/index.js
@@ -8,6 +8,36 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//
+const midGlb= function (req, res, next) {
+    console.log("Hi I am a GLOBAL middleware");
+
+    // console.log(req.ip); //::ip
+    // console.log(req.originalUrl)
+    //logic
+    next()    
+}
+// called as global middleware
+
+const assignmentMW=function(req,res,next){
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+let ip=req.ip
+let url=req.originalUrl
+
+// console.log('${datetime}, ${ip}, ${url}');
+console.log(datetime ,ip, url);
+
+next();
+}
+app.use(assignmentMW)
+//
 const mongoose = require('mongoose')
 
 mongoose.connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/<PrajaktaKadamDB>-database?retryWrites=true&w=majority", {useNewUrlParser: true})
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const UserController= require ("../controllers/usercontroller")
+
+const commonMW= require("../middlewares/commonMiddlewares")
 
 
 
-const myAuthorController=require("../controllers/myAuthorController")
-const myBookController=require("../controllers/myBookController")
-const publisherController=require("../controllers/publisherController")
+// const mid1= function (req, res, next) {
+//     console.log("Hi I am a middleware named Mid1 ");
+//     //logic
+//     let loggedIn= false
 
-router.post('/createmyAuthor', myAuthorController.createmyAuthor)
-router.post('/createMyBook', myBookController.createMyBook)
-router.post('/ bookwthauthorpublisher',myBookController.bookwthauthorpublisher)
-router.get('/getBooks', myBookController.getBooks)
+//     if(loggedIn==true) { 
+//         next()
+//     }
+//     else{
+//         res.send("Please login or register")
+//     }
+// }
 
-router.post('/createpublisher',publisherController.createpublisher)
+
+//raute middleware
+
+router.get('/basicRoute', commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4,  UserController.basicCode  );
+
+//e.g. restricted and open-to-al APIs can be handled like below now
+// router.get('/homePage', mid1,   UserController.feeds  );
+// router.get('/profileDetails', mid1,   UserController.profileDetails  );
+// router.get('/friendList', mid1,   UserController.friendList  );
+// router.get('/changePassword', mid1,   UserController.changePassword );
+
+// router.get('/termsAndConditions',  UserController.termsAndConditions );
+// router.get('/register',  UserController.register );
+
+
+
+
+// router.get('/basicRoute2', mid1,  UserController.basicCode2  );
+// router.get('/basicRoute3', mid1,  UserController.basicCode3  );
+// router.get('/basicRoute4', mid1,  UserController.basicCode4  );
+
+
+
+
+
+
+
+router.post('/createUser',UserController.createUser)
+router.get('/getUsersData', UserController .getUsersData)
+
 module.exports = router;
